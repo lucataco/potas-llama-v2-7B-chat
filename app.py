7e2764e587b284b41e8345aa050868a4c488b746
lucataco <lucataco@umd.edu>
2023-07-22 16:56:40 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from potassium import Potassium, Request, Response
+
+from transformers import pipeline
+import torch
+
+app = Potassium("my_app")
+
+# @app.init runs at startup, and loads models into the app's context
+@app.init
+def init():
+    device = 0 if torch.cuda.is_available() else -1
+    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+   
+    context = {
+        "model": model
+    }
+
+    return context
+
+# @app.handler runs for every call
+@app.handler("/")
+def handler(context: dict, request: Request) -> Response:
+    prompt = request.json.get("prompt")
+    model = context.get("model")
+    outputs = model(prompt)
+
+    return Response(
+        json = {"outputs": outputs[0]}, 
+        status=200
+    )
+
+if __name__ == "__main__":
+    app.serve()
